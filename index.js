67d798d8e03603e8b878b81505efa7dbb93fe48d
Mansi Padgelwar <74247233+mansipadgelwar@users.noreply.github.com>
2021-08-03 04:52:20 +0000

    feat: inverted star pattern

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var num = readlineSync.question("Enter a number: ");
 
 for(let i = 0;i < num;i++){
   var str = "";
-  for(let j=0;j<=i;j++){
+  for(let j=num-1;j>=i;j--){
     str = str + "* ";
   }
   console.log(str);
